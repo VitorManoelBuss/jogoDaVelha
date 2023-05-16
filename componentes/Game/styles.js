a1645a6ce01e5a88aba16695e80522946007feb7
VitorManoelBuss <107023990+VitorManoelBuss@users.noreply.github.com>
2023-05-16 20:32:59 -0300

    Eu sou o X

---
+++ componentes/Game/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  
+    titulo: {
+      fontSize: 32,
+      marginBottom: 24,
+    },
+
+    subtitulo: {
+      fontSize: 15,
+      marginBottom: 24,
+      textAlign: 'center',
+    },
+  
+    linha: {
+      display: 'flex',
+      flexDirection: 'row', 
+    },
+    tabuleiro: {
+      marginTop: 24,
+    },
+    areaReinicio: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    status: {
+      fontSize: 20,
+      marginBottom: 12,
+    },
+  
+  });
+
+  export default styles;
